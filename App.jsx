1745befe2ecbfa826f658dcd93ff285d710225f5
Joseph Korany <jkorany@gmail.com>
2016-07-23 23:26:14 -0700

    After tut #6 thinking with components

--- App.jsx
+++ App.jsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
+import ResolutionsForm from './ResolutionsForm.jsx';
+import ResolutionSingle from './ResolutionSingle.jsx';
 
 Resolutions = new Mongo.Collection("resolutions");
 
@@ -10,18 +12,6 @@ export default class App extends TrackerReact(React.Component) {
 
     resolutions() {
         return Resolutions.find().fetch();
-    }
-
-    addResolution(event) {
-        event.preventDefault();
-        var text = this.refs.resolution.value.trim();
-
-        Resolutions.insert({
-            text: text,
-            complete: false,
-            createdAt: new Date()
-        });
-        this.refs.resolution.value = "";
     }
 
     render() {
@@ -33,15 +23,10 @@ export default class App extends TrackerReact(React.Component) {
         return (
             <div>
                 <h1>My Resolutions</h1>
-                <form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
-                    <input
-                        type="text"
-                        ref="resolution"
-                        placeholder="Finish React Meteor Series" />
-                </form>
-                <div>
-                    {res[0].text}
-                </div>
+                <ResolutionsForm />
+                <ul>
+                    <ResolutionSingle resolution={res[0]} />
+                </ul>
             </div>
         )
     }
