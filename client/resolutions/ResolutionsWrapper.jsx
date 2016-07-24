ffd5e1208b0a3999629b4a9e03935dde099b70ef
Joseph Korany <jkorany@gmail.com>
2016-07-23 23:52:25 -0700

    After tutorial 7, cleaning up and stylizing

--- client/resolutions/ResolutionsWrapper.jsx
+++ client/resolutions/ResolutionsWrapper.jsx
@@ -3,12 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
+
 import ResolutionsForm from './ResolutionsForm.jsx';
 import ResolutionSingle from './ResolutionSingle.jsx';
 
 Resolutions = new Mongo.Collection("resolutions");
 
-export default class App extends TrackerReact(React.Component) {
+export default class ResolutionsWrapper extends TrackerReact(React.Component) {
 
     resolutions() {
         return Resolutions.find().fetch();
@@ -19,7 +20,6 @@ export default class App extends TrackerReact(React.Component) {
         if(res.length < 1) {
             return (<div>Loading</div>)
         }
-        console.log(this.resolutions());
         return (
             <div>
                 <h1>My Resolutions</h1>
